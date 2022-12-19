166cb384c04a10c745cdd64aa3388973f5776e39
TommasoUrsomando <115471825+TommasoUrsomando@users.noreply.github.com>
2022-12-19 13:36:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
 using namespace std;
+#include "math.h"
+
+
+float f(float y){
+    float risultato;
+    risultato = pow(y, 2)* cos(y)+1;
+    return risultato;
+}
+
+
 
 int main() {
-   return 0;
+    float a, b;
+
+    do{
+        cout << "inserire estremi" << endl;
+        cin >> a >> b;
+        }while(f(a)*f(b)>=0);
+    float x, err;
+
+    do{
+        x=(a+b)/2;
+
+        if(f(x)==0){
+            cout<< x<< endl;
+            cout << f(x);
+            return 0;
+        }
+        else{
+            if(f(a)*f(x)<0){
+                b=x;
+            }
+            else {
+                a=x;
+            }
+            err=abs((b-a)/2);
+        }
+  } while (err>=1e-6);
+    cout<<int(x*10000)/10000.0<<endl;
+
+    return 0;
 }
